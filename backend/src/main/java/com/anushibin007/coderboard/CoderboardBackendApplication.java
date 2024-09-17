ab4dba36769c196588a8d4142840dfea4df896e4
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2024-09-17 16:38:50 +0530

    🥇 First commit

---
+++ backend/src/main/java/com/anushibin007/coderboard/CoderboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.anushibin007.coderboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderboardBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/anushibin007/coderboard/CoderboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anushibin007.coderboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderboardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
